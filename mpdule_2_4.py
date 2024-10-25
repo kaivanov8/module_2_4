289f623a9b925ecb16695868d5d8bf7fd883a70d
kaivanov8 <kosiv6799@gmail.com>
2024-10-25 20:24:39 +0700

    Add files via upload

---
+++ mpdule_2_4.py
@@ -0,0 +1,16 @@
+# цикл for
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+primes = []
+not_primes =[]
+for i in range(1,len(numbers)): # i- индекс списка со значениями от 1 до 14
+    is_prime = True
+    for j in range(2,numbers[i]): # j- делитель для проверки остатка деления
+        if numbers[i] % j == 0:
+            is_prime = False
+            break
+    if is_prime :
+        primes.append(numbers[i])
+    else:
+        not_primes.append(numbers[i])
+print ('Primes:', primes)
+print('Not primes:', not_primes)
